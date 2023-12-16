8bd35328a94e35954d2c46f48cc3d8901d6f7845
Han Wong <han.wong@pm.me>
2023-12-16 19:05:19 +0100

    add test app

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+from board import pages
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(pages.bp)
+    return app
+
+if __name__ == "__main__":
+    create_app().run() 
