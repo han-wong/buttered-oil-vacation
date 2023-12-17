0166f4258d74129d71ab55f76c4b2aeffd032f2d
Han Wong <han.wong@pm.me>
2023-12-17 13:59:35 +0100

    test push

--- __init__.py
+++ __init__.py
@@ -7,16 +7,13 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     print(config_class)
+    
     # Initialize Flask extensions here
 
     # Register blueprints here
     from flask_hangman.main import bp as main_bp
 
     app.register_blueprint(main_bp)
-
-    @app.route("/test/")
-    def test_page():
-        return "<h1>Testing the Flask Application Factory Pattern</h1>"
 
     return app
 
