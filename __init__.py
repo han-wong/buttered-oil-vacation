755ae4e8cd3c087ce9e343bb0bbcd4102c2fb65e
Han Wong <han.wong@pm.me>
2023-12-17 14:01:46 +0100

    test push

--- __init__.py
+++ __init__.py
@@ -7,7 +7,6 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     print(config_class)
-    
     # Initialize Flask extensions here
 
     # Register blueprints here
