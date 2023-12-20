8d325f946cd5a7e1dff1fa6431d8967bf842ec92
Han Wong <han.wong@pm.me>
2023-12-21 00:49:05 +0100

    updated navigation

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cd buttered-oil-vacation
+# Install modules
+# python3 -m pip install -r requirements.txt
+
+# Initialize database or reset
+# python3 -m flask --app board init-db
 
 # For development use (simple logging, etc):
-# python3 -m pip install -r requirements.txt
-# python3 server.py
+# python3 -m flask --app board run --port 8000 --debug
 
 # For production use: 
-gunicorn server:app -w 1 --log-file -
+python3 -m gunicorn "board:create_app()" -w 1 --log-file -
